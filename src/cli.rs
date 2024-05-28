4cb1e9552a35c8c76ac1dea58058e7d32a4820ee
Sab Natarajan <sab.natarajan@gmail.com>
2024-05-27 23:56:51 -0400

    v0.2.0
    * Parse milliseconds, microseconds and nanoseconds too
    * Case-insensitive parsing of timezones

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
-use structopt::StructOpt;
-use chrono::{Local, NaiveDateTime, TimeZone};
+use chrono::{DateTime, Local};
 use chrono_tz::Tz;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 enum Everytime {
@@ -11,45 +11,54 @@ enum Everytime {
 
         /// Display as time since the Unix Epoch
         #[structopt(short)]
-        epoch: bool
+        epoch: bool,
     },
     /// Parse a timestamp to the current local time
     Parse {
         /// The timestamp (in milliseconds) to parse
-        parse_str: String
+        parse_str: String,
     },
 }
 
 fn now(tz: Option<String>, epoch: bool) -> String {
     let tz = match tz {
         Some(s) => s,
-        None => "".to_string()
+        None => "".to_string(),
     };
 
-    let t:Result<Tz,String> = tz.parse();
+    let t = Tz::from_str_insensitive(tz.as_str());
     let now = Local::now();
     match t {
         Ok(t) => match epoch {
             true => now.with_timezone(&t).timestamp().to_string(),
-            false => now.with_timezone(&t).to_string()
+            false => now.with_timezone(&t).to_string(),
         },
         Err(_) => match epoch {
             true => now.timestamp().to_string(),
-            false => now.to_string()
-        }
+            false => now.to_string(),
+        },
     }
 }
 
-fn parse(parse_str: String) {
-    let parse_millis: Result<i64, _> = parse_str.parse();
-    match parse_millis {
-        Ok(millis) => {
-            let nsecs = 0;
-            let ndt = NaiveDateTime::from_timestamp(millis, nsecs);
-            let ldt = Local.from_utc_datetime(&ndt).to_rfc3339();
-            println!("{}", ldt);
-        },
-        Err(_) => eprintln!("Could not parse")
+fn parse(timestamp: String) {
+    let ts = timestamp.parse::<i64>().expect("Unable to parse");
+    let mut local_dt = None;
+
+    // h/t epochconverter.com
+    if ts >= 1e8 as i64 && ts < 18e7 as i64 {
+        eprintln!("Expected a more recent date. You are missing 1 digit");
+    } else if ts >= 1e14 as i64 || ts <= -1e14 as i64 {
+        local_dt = DateTime::from_timestamp_micros(ts);
+    } else if ts >= 1e11 as i64 || ts <= -3e10 as i64 {
+        local_dt = DateTime::from_timestamp_millis(ts);
+    } else {
+        local_dt = DateTime::from_timestamp(ts, 0);
+    }
+
+    if let Some(dt) = local_dt {
+        println!("{}", DateTime::<Local>::from(dt));
+    } else {
+        eprintln!("unknown error while parsing");
     }
 }
 
